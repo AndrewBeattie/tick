47fdd4775a4d2813f46cca3221213c86f5b2ef0b
andrew <andrew.beattie@farmersedge.ca>
2022-12-11 17:14:21 -0700

    Starting the update to modern repo

---
+++ tick_harness.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append("/home/andrew/Projects/tick/")
+from tickspot.net.tickspot import TickSpot
+
+TickSpot()
---
+++ tickspot/manual_settings.py
@@ -0,0 +1,5 @@
+class Constants:
+  BaseUrl = "https://www.tickspot.com/api/v2/"
+  BaseUrlSubId = "https://www.tickspot.com/%s/api/v2/"
+  DateFormat = "%Y-%m-%d"
+  
