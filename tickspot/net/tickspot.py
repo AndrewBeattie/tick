288640fef930eff72df4fc4d2ecfc06c4a91b0ad
andrewbeattie <andrew.beattie@farmersedge.ca>
2022-12-30 16:42:34 -0700

    test

--- tickspot/net/tickspot.py
+++ tickspot/net/tickspot.py
@@ -14,43 +14,62 @@ date : summary of changes
 31/07/2020: Creating base classes and models for GET and POST requests
         - https://github.com/tick/tick-api/blob/master/sections/entries.md
 """
-import json
+import time
+import datetime
 from pprint import PrettyPrinter
 from tickspot.settings import env
-from tickspot.net.network import Entry, Project, Task, Authorize
+from tickspot.net.service import Entry, Project, Task, Authorize
+
 
 def current_date():
-    import datetime
     return datetime.datetime.now().strftime("%Y-%m-%d")
+
 
 class TickSpot(Authorize):
     def __init__(self, username: str, password: str):
         super(TickSpot, self).__init__(username, password)
-        self.entry = Entry(self.sub_id, self.token)
-        self.task = Task(self.sub_id, self.token)
-        self.project = Project(self.sub_id, self.token)
+        self.user = username
+        self.entry = Entry(self.token, self.user, self.sub_id)
+        self.task = Task(self.token, self.user, self.sub_id)
+        self.project = Project(self.token, self.user, self.sub_id)
         self.pp = PrettyPrinter(indent=4)
 
     def list_tasks(self, project_id: int):
-        self.pp.pprint(self.task.list(project_id=project_id))
+        tasks = self.task.list(project_id=project_id)
+        for task in tasks:
+            print(f"Task Name: {task.get('name')}")
+            print(f"       Id: {task.get('id')}")
 
     def list_projects(self):
-        self.pp.pprint(self.project.list())
+        projects = self.project.list()
+        for project in projects:
+            print(f"Project Name: {project.get('name')}")
+            print(f"          Id: {project.get('id')}")
 
     def list_entries(self, project_id: int, start_date: str, end_date: str):
         self.pp.pprint(
             self.entry.list(project_id=project_id, start_date=start_date, end_date=end_date)
         )
-    
+
     def list_entries_today(self, project_id: int):
-        entries = self.entry.list(project_id=project_id, start_date=current_date(), end_date=current_date())
-        self.pp.pprint(
-            entries
-        )
+        if not project_id:
+            entries = []
+            projects = self.project.list()
+            project_ids = [project.get("id") for project in projects]
+            for project_id in project_ids:
+                entries += self.entry.list(project_id=project_id,
+                                           start_date=current_date(), end_date=current_date())
+        else:
+            entries = self.entry.list(project_id=project_id,
+                                      start_date=current_date(), end_date=current_date())
         hours = 0
+        print("---------------------------------------------")
         for entry in entries:
+            print(f"Hours: {entry.get('hours')}")
+            print(f"       {entry.get('notes')}")
+            print("---------------------------------------------")
             hours += entry.get("hours")
-        print(f"Hours: {hours}")
+        print(f"Hours Remaining: {8-hours}")
 
     def create_entry(self, project_id: int, task_id: int, hours: float, date: str, note: str):
         self.entry.post(
@@ -72,10 +91,10 @@ def fetch(args):
         tickspot.list_projects()
     elif args.category == "task":
         tickspot.list_tasks(project_id=args.project)
-    elif args.category == "today":
+    elif args.category == "time":
         tickspot.list_entries_today(project_id=args.project)
     else:
-        raise ValueError("Only project and task are supported for list.")
+        raise ValueError("Only project, task and time are supported for list.")
 
 
 def create(args):
@@ -95,19 +114,31 @@ def create(args):
         project_id=args.project, task_id=args.task, hours=args.hours, date=args.date, note=None
     )
 
+
 def start(args):
-    import time
-    import datetime
     start_time = time.time()
     if not args.project:
         raise ValueError("Require project")
     if not args.task:
         raise ValueError("Require task")
-    input("Press Enter to stop")
-    end_time = time.time()
+    if args.message:
+        print(f"Working on: {args.message}")
+        input("     Press Enter to Submit Time: ")
+    else:
+        try:
+            import os 
+            from git import Repo
+            repo = Repo(os.getcwd())
+            branch = repo.active_branch.name
+            args.message = branch
+            print(f"Working on: {args.message}")
+            input("     Press Enter to Submit Time: ")
+        except:
+            args.message = input("     Enter Note to Submit Time: ")
 
+    end_time = time.time()
     time_lapsed = end_time - start_time
-    hours = time_lapsed/ 3600.0
+    hours = time_lapsed / 3600.0
 
     tickspot = TickSpot(
         username=env.get("TICKSPOT_USERNAME"), password=env.get("TICKSPOT_PASSWORD")
--- tickspot/net/timer.py
+++
@@ -1,2 +0,0 @@
-# ticker start -p 2162959 -t 16593152 -m testing
-# ticker list today -p 2162959
